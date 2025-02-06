2b9b6c0d0bc3dc951156c424b75283e5ceb8f2f1
Damdev80 <danielbuelvas9624@gmail.com>
2025-02-06 16:53:37 -0500

    Realize el ejercicio 3

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
+
+
+
+
 
 app = Flask(__name__)
 
@@ -23,6 +27,18 @@ def saludar(nombre):
 def suma(n1,n2):
     resultado = n1 + n2
     return f"El resultado es {resultado}"
+
+#Ejercicio 3
+@app.route('/info',  methods=['GET'])
+def infomation():
+    show_header = request.headers
+    show_conexion_sure = request.is_secure
+    return jsonify({"headers": dict(show_header), "is_secure": show_conexion_sure})
+
+
+    
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
